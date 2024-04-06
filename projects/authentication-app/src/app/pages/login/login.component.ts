ba38e3b8ff359e3686e6d9d198c2d39f23f65ef2
harley santos garzon <harley.santos.garzon@gmail.com>
2024-04-06 11:03:32 -0500

    refactor(pages): use login-form component

--- projects/authentication-app/src/app/pages/login/login.component.ts
+++ projects/authentication-app/src/app/pages/login/login.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { compareControlsValidator, passwordValidator } from '@shared/validator';
+
+/**
+ * Login Flow / Payload
+ */
+export type LoginFormControl = {
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'auth-login',
@@ -7,4 +17,35 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public formLoginGroup!: FormGroup<LoginFormControl>
+
+  constructor() {
+    const formGroupConfig = {
+      validators: [
+        // compareControlsValidator('password', 'confirmPassword')
+      ]
+    }
+    const controls = {
+      email: new FormControl('', [
+        Validators.required,
+        // TODO: emailValidator()
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        // passwordValidator()
+        // TODO: passwordValidator()
+      ])
+    }
+    this.formLoginGroup = new FormGroup(controls, formGroupConfig)
+  }
+
+  submit(): void {
+    if (this.formLoginGroup.valid) {}
+
+    const { email, password } = this.formLoginGroup.value
+    console.log('👨‍🚀 ~ LoginComponent ~ submit:', {
+      isValid: this.formLoginGroup.valid,
+      email, password
+    })
+  }
 }
--- projects/authentication-app/src/app/pages/login/login.module.ts
+++ projects/authentication-app/src/app/pages/login/login.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgTemplateOutlet } from '@angular/common';
+
+import { BrandComponent } from '../../components/atoms/brand/brand.component';
+import { InputComponent } from '../../components/atoms/input/input.component';
 import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login.routing.module';
+import { LoginFormComponent } from '../../components/molecules/login-form/login-form.component';
 
 @NgModule({
-  imports: [],
+  imports: [RouterLink, NgTemplateOutlet, ReactiveFormsModule, LoginRoutingModule, BrandComponent, InputComponent, LoginFormComponent],
   exports: [],
   declarations: [LoginComponent],
   providers: [],
