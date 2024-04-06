8d10c9b71f4180ea38381320766d4893e41e9475
harley santos garzon <harley.santos.garzon@gmail.com>
2024-04-06 11:03:09 -0500

    refactor(pages): make base structure

--- projects/authentication-app/src/app/pages/home/home.component.ts
+++ projects/authentication-app/src/app/pages/home/home.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrandComponent } from '../../components/atoms/brand/brand.component';
+import { InputComponent } from '../../components/atoms/input/input.component';
+import { compareControlsValidator, passwordValidator } from '@shared/validator'
+
+/**
+ * Login Flow / Payload
+ */
+export type LoginFormControl = {
+  email: FormControl,
+  password: FormControl,
+  confirmPassword: FormControl,
+}
 
 @Component({
   selector: 'auth-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule, BrandComponent, InputComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  public formLoginGroup!: FormGroup<LoginFormControl>
+
+  constructor() {
+    const formGroupConfig = {
+      validators: [
+        compareControlsValidator('password', 'confirmPassword')
+      ]
+    }
+    const controls = {
+      email: new FormControl('', [
+        Validators.required,
+        // TODO: emailValidator()
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        passwordValidator()
+        // TODO: passwordValidator()
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required
+      ]),
+    }
+    this.formLoginGroup = new FormGroup(controls, formGroupConfig)
+  }
 }
