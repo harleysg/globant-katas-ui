4c7afb83f4f983828ab3611f1650660245cf77a8
harley santos garzon <harley.santos.garzon@gmail.com>
2024-04-06 11:03:52 -0500

    refactor(root): include main routes

--- projects/authentication-app/src/app/app.routes.ts
+++ projects/authentication-app/src/app/app.routes.ts
@@ -1,4 +1,25 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    title: 'Home',
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(comp => comp.HomeComponent)
+  },
+  {
+    title: 'login',
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(mod => mod.LoginModule)
+  },
+  {
+    title: 'register',
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.component').then(comp => comp.RegisterComponent)
+  },
+  {
+    title: 'profile',
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.component').then(comp => comp.ProfileComponent)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
