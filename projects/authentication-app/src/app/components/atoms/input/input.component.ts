1040ccc58d9dc2c9b63ab2303f88eec19a2ac7e3
harley santos garzon <harley.santos.garzon@gmail.com>
2024-04-06 11:01:18 -0500

    refacotr(components): general improving

--- projects/authentication-app/src/app/components/atoms/input/input.component.ts
+++ projects/authentication-app/src/app/components/atoms/input/input.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'auth-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
-export class InputComponent {
+export class InputComponent implements OnChanges {
+  @Input() autocomplete: boolean = false;
+  @Input() control: FormControl = new FormControl();
+  @Input() pattern: string = '';
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() value: string = '';
+  @Input() label: string = '';
+  @Input() image: string = '';
+  @Input() name: string = '';
+  @Input() invalid: boolean | undefined = false;
+  @Input() required: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['value'] && changes['value'].currentValue) {
+      this.control.setValue(this.value)
+    }
+  }
 }
