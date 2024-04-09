971517ac5572a6fb5eb6f16728ff3e63300ecf94
harley santos garzon <harley.santos.garzon@gmail.com>
2024-04-09 17:52:46 -0500

    refactor(pages) use new wrapper component

--- projects/authentication-app/src/app/pages/register/register.page.component.ts
+++ projects/authentication-app/src/app/pages/register/register.page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SharedModule } from '@shared/shared.module';
 
 @Component({
   selector: 'auth-register',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, RouterLink],
   templateUrl: './register.page.component.html',
   styleUrl: './register.page.component.scss'
 })
--- projects/authentication-app/src/app/shared/shared.module.ts
+++ projects/authentication-app/src/app/shared/shared.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrandComponent } from './components/atoms/brand/brand.component';
 import { ButtonComponent } from './components/atoms/button/button.component';
 import { InputComponent } from './components/atoms/input/input.component';
-import { LoginFormComponent } from './components/molecules/login-form/login-form.component';
+import { WrapperComponent } from './components/molecules/wrapper/wrapper.component';
+import { SocialProfileComponent } from './components/molecules/social-profile/social-profile.component';
+
+const atoms = [BrandComponent, ButtonComponent, InputComponent]
+const molecules = [WrapperComponent, SocialProfileComponent]
 
 @NgModule({
-  imports: [BrandComponent, ButtonComponent, InputComponent, LoginFormComponent],
-  exports: [BrandComponent, ButtonComponent, InputComponent, LoginFormComponent],
+  imports: [...atoms, ...molecules],
+  exports: [...atoms, ...molecules],
   providers: [],
 })
 export class SharedModule { }
