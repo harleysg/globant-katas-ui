156e45e3d539d540981812296a29dc103be1b6b0
harley santos garzon <harley.santos.garzon@gmail.com>
2024-05-17 18:20:53 -0500

    styles(*): adjust line

--- projects/authentication-app/src/app/app.config.ts
+++ projects/authentication-app/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// -----
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
