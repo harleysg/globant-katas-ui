86073e4e3e8ef7f3218d0543f695728439b16d0a
harley santos garzon <harley.santos.garzon@gmail.com>
2024-05-17 18:19:55 -0500

    refactor(routes): use layout, resolver and guard

--- projects/authentication-app/src/app/app.routes.ts
+++ projects/authentication-app/src/app/app.routes.ts
@@ -1,56 +1,38 @@
-import { Injectable, inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+// -----
+import { loggedGuard, publicGuard } from '@shared/guard/logged/logged.guard';
+import { userResolver } from '@shared/resolvers/user/user-resolver.resolver';
+// -----
 import { PrivateLayoutComponent } from '@shared/components/template/layout/private-layout/private-layout.component';
 import { PublicLayoutComponent } from '@shared/components/template/layout/public-layout/public-layout.component';
 
-// Services
-@Injectable()
-class UserToken {}
-
-@Injectable()
-class PermissionsService {
-  canActivate(currentUser: UserToken, userId: string): boolean {
-    // Validar toker | session | dbSession
-    return false;
-  }
-  canMatch(currentUser: UserToken): boolean {
-    return true;
-  }
-}
-
-const canActivateTeam: CanActivateFn = (
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot,
-) => {
-  const { canActivate } = inject(PermissionsService)
-  return canActivate(inject(UserToken), '1');
-};
-
 export const routes: Routes = [
   {
-    title: 'Home',
     path: '',
     component: PublicLayoutComponent,
-    children: [{
-      path: '',
-      loadComponent: () => import('./pages/home/home.page.component').then(comp => comp.HomePageComponent)
-    }]
-  },
-  {
-    title: 'Register',
-    path: 'register',
-    component: PublicLayoutComponent,
-    children: [{
-      path: '',
-      loadComponent: () => import('./pages/register/register.page.component').then(comp => comp.RegisterPageComponent)
-    }]
+    canActivate: [publicGuard],
+    children: [
+      {
+        title: 'Home',
+        path: '',
+        loadComponent: () => import('./pages/home/home.page.component').then(comp => comp.HomePageComponent)
+      },
+      {
+        title: 'Register',
+        path: 'register',
+        loadComponent: () => import('./pages/register/register.page.component').then(comp => comp.RegisterPageComponent)
+      }
+    ]
   },
   {
     // Private
     title: 'Profile',
     path: 'profile',
-    // canActivate: [canActivateTeam], // Guard
     component: PrivateLayoutComponent,
+    canActivate: [loggedGuard],
+    resolve: {
+      profile: userResolver
+    },
     children: [
       {
         path: '',
