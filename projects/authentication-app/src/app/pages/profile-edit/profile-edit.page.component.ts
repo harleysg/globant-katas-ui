efeb1b5179fec342f4628907947ddc2c5e034327
harley santos garzon <harley.santos.garzon@gmail.com>
2024-05-17 22:46:24 -0500

    refactor(page): replace static form with user service

--- projects/authentication-app/src/app/pages/profile-edit/profile-edit.page.component.ts
+++ projects/authentication-app/src/app/pages/profile-edit/profile-edit.page.component.ts
@@ -1,117 +1,57 @@
-import { Component, ElementRef, OnInit, inject, viewChild } from '@angular/core';
-import { JsonPipe, Location, NgTemplateOutlet } from '@angular/common';
-import { FormArray, FormBuilder, FormControl, ValidatorFn, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { Location } from '@angular/common';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 // -----
 import { SharedModule } from '@shared/shared.module';
-// -----
-const TYPES = {
-  TEXTAREA: 'textArea',
-  BUTTON: 'button',
-  CHECKBOX: 'checkbox',
-  COLOR: 'color',
-  DATE: 'date',
-  DATETIME_LOCAL: 'datetime-local',
-  EMAIL: 'email',
-  FILE: 'file',
-  HIDDEN: 'hidden',
-  IMAGE: 'image',
-  MONTH: 'month',
-  NUMBER: 'number',
-  PASSWORD: 'password',
-  RADIO: 'radio',
-  RANGE: 'range',
-  RESET: 'reset',
-  SEARCH: 'search',
-  SUBMIT: 'submit',
-  TEL: 'tel',
-  TEXT: 'text',
-  TIME: 'time',
-  URL: 'url',
-  WEEK: 'week',
-} as const
-type Types = typeof TYPES[keyof typeof TYPES]
-/**
- * Login Flow / Payload
- */
-export type EditFormControl = {
-  photo: FormArray,
-  name: FormArray,
-  bio: FormArray,
-  phone: FormArray,
-  email: FormArray,
-  password: FormArray
-}
-// -----
+import { HTMLInputTypes, UserInfo } from '@shared/types';
+import { UserService } from '@shared/service/user/user.service';
+
 @Component({
   selector: 'auth-profile-edit',
   standalone: true,
-  imports: [
-    RouterLink,
-    NgTemplateOutlet,
-    ReactiveFormsModule,
-    SharedModule,
-    JsonPipe
-  ],
+  imports: [RouterLink, ReactiveFormsModule, SharedModule],
   templateUrl: './profile-edit.page.component.html',
   styleUrl: './profile-edit.page.component.scss'
 })
 export class ProfileEditPageComponent implements OnInit {
+  private route = inject(ActivatedRoute)
   private location = inject(Location)
-  private formBuilder = inject(FormBuilder)
-  public imageRef = viewChild<ElementRef>('imageRef')
-  public controls: {
-      name: string;
-      control: FormControl;
-      type: Types,
-      accept?: string
-  }[] = []
-  public userData = [
-    {type: TYPES.TEXT, accept: "image/*", key: 'photo', value: 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'},
-    {type: TYPES.TEXT, key: 'name', value: 'Alma Sofia Santos Soto'},
-    {type: TYPES.TEXTAREA, key: 'bio', value: 'I am little baby closing to arrive to this world... I`m exiting to know my parents, thay are a software developer and a big fan of devchallenges'},
-    {type: TYPES.TEXT, key: 'phone', value: '3502342343'},
-    {type: TYPES.TEXT, key: 'email', value: 'almaSofiaSS@gmail.com'},
-    {type: TYPES.PASSWORD, key: 'password', value: '123456'},
-  ] as {
+  private userService = inject(UserService)
+  public userData = [] as {
       key: string;
       value: string;
-      type: Types,
+      type: HTMLInputTypes,
       accept?: string,
       control: FormControl;
   }[]
-  public form = this.formBuilder.group({})
+  public form!: FormGroup<{}>
+
+  public user?: UserInfo
 
   ngOnInit(): void {
-    this.buildForm()
+    this.readUserData()
   }
 
-  private buildForm(): void {
-    for (const [index, {key, value, type, accept}] of this.userData.entries()) {
-      const required = Validators.required
-      const validator: ValidatorFn[] | null = []
-      const notRequired = ['photo', 'bio', 'phone']
-      
-      if (!notRequired.includes(key)) {
-        validator.push(required)
-      }
+  private readUserData() {
+    this.route.data.subscribe(response => {
+      this.user = response['profile']
 
-      this.form.addControl(key, this.formBuilder.control(value, validator))
+      if (!this.user) return
 
-      this.controls.push({
-        name: key,
-        control: this.formBuilder.control(value, validator),
-        type: type as Types,
-        accept
-      })
-      this.userData[index].control = this.formBuilder.control(value, validator)
-    }
+      this.buildForm(this.user)
+    })
+  }
+
+  private buildForm(user: UserInfo): void {
+    let { form, userData } = this.userService.buildForm(true, user)
+
+    this.form = form
+    this.userData = userData
   }
 
   submit(): void {
-    if (this.form.valid) {}
-
     const value = this.form.value
     console.log('👨‍🚀 ~ LoginComponent ~ submit:', {
       isValid: this.form.valid,
@@ -122,21 +62,10 @@ export class ProfileEditPageComponent implements OnInit {
   back(): void {
     this.location.back()
   }
-  
-  onChangeImage($event: Event) {
-    const _this = this
-    const { target } = $event as InputEvent
-    const { files } = target as HTMLInputElement
-    const image = this.imageRef()?.nativeElement
-    const reader = new FileReader();
 
-    if (!files && !files?.[0]) return
+  onChangeImage(file: string | ArrayBuffer | null) {
+    if (!file) return
 
-    reader.onload = function (event) {
-      const currentTarget = event?.currentTarget as FileReader
-      image.src = currentTarget.result?.toString() || '';
-      _this.form.get('photo')?.patchValue(currentTarget.result as never)
-    }
-    reader.readAsDataURL(files[0]);
+    this.form.get('photo')?.patchValue(file as never)
   }
 }
--- projects/authentication-app/src/app/pages/register/register.page.component.ts
+++ projects/authentication-app/src/app/pages/register/register.page.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+// -----
+import { UserService } from '@shared/service/user/user.service';
 import { SharedModule } from '@shared/shared.module';
+import { HTMLInputTypes } from '@shared/types';
 
 @Component({
   selector: 'auth-register',
   standalone: true,
-  imports: [SharedModule, RouterLink],
+  imports: [SharedModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register.page.component.html',
   styleUrl: './register.page.component.scss'
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+  public userData = [] as {
+      key: string;
+      value: string;
+      type: HTMLInputTypes,
+      accept?: string,
+      control: FormControl
+  }[]
+  public form!: FormGroup<{}>
+  private userService = inject(UserService)
 
+  ngOnInit(): void {
+    this.buildForm()
+  }
+
+  private buildForm(): void {
+    const { form, userData } = this.userService.buildForm(false)
+
+    this.form = form
+    this.userData = userData
+  }
+
+  onChangeImage(file: string | ArrayBuffer | null) {
+    if (!file) return
+
+    this.form.get('photo')?.patchValue(file as never)
+  }
+
+  submit(): void {
+    const value = this.form.value
+    console.log('👨‍🚀 ~ LoginComponent ~ submit:', {
+      isValid: this.form.valid,
+      ...value
+    })
+  }
 }
